bb81cc85b6eb4cd8b091ee2b1d14f2aa05186dd7
ytw9699 <ytw9699@naver.com>
2018-11-12 18:54:03 +0900

    1부터 n까지합

--- algorithm/Do_it_dataStructure&algorithm/a_기본알고리즘/j_SumWhile_1부터n까지의합.java
+++ algorithm/Do_it_dataStructure&algorithm/a_기본알고리즘/j_SumWhile_1부터n까지의합.java
@@ -19,5 +19,6 @@ class j_SumWhile_1부터n까지의합 {
 			i++;					// i 값을 1만큼 증가시킵니다.
 		}
 		System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+		System.out.println("i의 값은 " + i + "가(이) 되었습니다.");
 	}
 }
