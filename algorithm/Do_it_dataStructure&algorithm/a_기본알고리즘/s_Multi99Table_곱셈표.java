c155854f1dec36ae14ece6e00c3c2fef0f2e74bc
ytw9699 <ytw9699@naver.com>
2018-11-13 19:05:07 +0900

    곱셈표

---
+++ algorithm/Do_it_dataStructure&algorithm/a_기본알고리즘/s_Multi99Table_곱셈표.java
@@ -0,0 +1,14 @@
+package a_±âº»¾Ë°í¸®Áò;
+//°ö¼ÀÇ¥¸¦ Ãâ·ÂÇÕ´Ï´Ù.
+public class s_Multi99Table_°ö¼ÀÇ¥ {
+	public static void main(String[] args) {
+		System.out.println("----- °ö¼ÀÇ¥ -----");
+
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				System.out.printf("%3d", i * j);
+			}
+			System.out.println();
+		}
+	}
+}
