0593428e16004cddb990e7ea7642ec508ee33286
ytw9699 <ytw9699@gmail.com>
2018-11-15 10:06:03 +0900

    별모양의 정사각형

---
+++ algorithm/Do_it_dataStructure&algorithm/a_기본알고리즘/t_Square_01_14.java
@@ -0,0 +1,22 @@
+package a_기본알고리즘;
+import java.util.Scanner;
+// 정사각형 모양을 나타냄
+
+public class t_Square_01_14 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		int n;
+
+		System.out.println("정사각형 모양으로 나타냅니다.");
+		do {
+			System.out.print("단수는：");
+			n = stdIn.nextInt();
+		} while (n <= 0);
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++)
+				System.out.print('*');
+			System.out.println();
+		}
+	}
+}
