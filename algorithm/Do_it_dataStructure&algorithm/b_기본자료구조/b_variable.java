d26a3562d1cd4d160a327f7e1a31faea9b28b28f
ytw9699 <ytw9699@gmail.com>
2018-11-15 19:46:34 +0900

    변수의 초기값 출력

---
+++ algorithm/Do_it_dataStructure&algorithm/b_기본자료구조/b_variable.java
@@ -0,0 +1,10 @@
+package b_기본자료구조;
+//구성 요소의 자료형이 int형인 배열 (구성 요솟수는  5 : new에 의해  본체를 생성）
+class b_variable {
+	static int a;
+	public static void main(String[] args) {
+		int b;
+		//System.out.println(b);//지역변수는 아래처럼 초기값넣어서 출력안됨
+		System.out.println(a);
+	}
+}
