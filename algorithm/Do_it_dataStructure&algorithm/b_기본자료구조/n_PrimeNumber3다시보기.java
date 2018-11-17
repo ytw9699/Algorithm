985cbbab4d22fb048fdf91356ab6e79699d722f7
ytw9699 <ytw9699@gmail.com>
2018-11-17 22:29:03 +0900

    개선된 소수구하기3번째

--- algorithm/Do_it_dataStructure&algorithm/b_기본자료구조/n_PrimeNumber3다시보기.java
+++ algorithm/Do_it_dataStructure&algorithm/b_기본자료구조/n_PrimeNumber3다시보기.java
@@ -1,14 +1,13 @@
 package b_기본자료구조;
 // 1,000 이하의 소수를 열거(버전 3)
-
-class PrimeNumber3 {
+class n_PrimeNumber3다시보기 {
 	public static void main(String[] args) {
 		int counter = 0;							// 곱셈과 나눗셈의 횟수
 		int ptr = 0;								// 찾은 소수의 개수
 		int[] prime = new int[500];					// 소수를 저장하는 배열
 
-		prime[ptr++] = 2;							// ２는 소수임
-		prime[ptr++] = 3;							// ３은 소수임
+		prime[ptr++] = 2;	//0번재		// ２는 소수임
+		prime[ptr++] = 3;	//1번째		// ３은 소수임
 
 		for (int n = 5 ; n <= 1000; n += 2) {		// 대상은 홀수만
 			boolean flag = false;
@@ -24,7 +23,6 @@ class PrimeNumber3 {
 				counter++;
 			}
 		}
-
 		for (int i = 0; i < ptr; i++)				// 찾은 ptr개의 소수를 출력
 			System.out.println(prime[i]);
 
