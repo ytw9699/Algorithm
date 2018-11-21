ae18c47807d7634879ebc78448ea470c7a463ffb
ytw9699 <ytw9699@gmail.com>
2018-11-21 19:41:30 +0900

    .

--- algorithm/CodingInterview/Ch_01_Arrays_and_Strings/Question02_1.java
+++ algorithm/CodingInterview/Ch_01_Arrays_and_Strings/Question02_1.java
@@ -1,7 +1,7 @@
 package Ch_01_Arrays_and_Strings;
 //순열이란?서로 다른 n개에서 r개를 택하여 일렬로 배열하는 것을 n개에서 r개를 택하는 순열
 //두문자열에서 순열관계란?두 문자열의 길이가 같고, 순서만 다른거 ,
-//공백도 문자하나로 취급 길이에 포함되고, 대문자도 구분한다
+//1공백도 문자하나로 취급 길이에 포함되고, 대문자도 구분한다
 public class Question02_1 {	
 	
 	public static String sort(String s) {
