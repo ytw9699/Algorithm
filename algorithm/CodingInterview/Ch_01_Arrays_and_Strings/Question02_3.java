46b9dbb947bb36011fcc42f6f7c2d80eb601614a
ytw9699 <ytw9699@gmail.com>
2018-11-21 16:57:34 +0900

    주석

---
+++ algorithm/CodingInterview/Ch_01_Arrays_and_Strings/Question02_3.java
@@ -0,0 +1,26 @@
+package Ch_01_Arrays_and_Strings;
+
+public class Question02_3 {	//입력값 모든 순열 구하기
+	
+	public static void permutation(String str) { 
+	    permutation("", str); 
+	}
+
+	private static void permutation(String prefix, String str) {
+		
+	    int n = str.length();
+	    
+	    if (n == 0) 
+	    	System.out.println(prefix);
+	    else {
+	        for (int i = 0; i < n; i++) {
+	    permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+	        }
+	    }
+	}
+		public static void main(String[] args) {
+			
+			permutation("abc");
+			
+			}
+	}
