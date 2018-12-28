4eb6844d486600395e197d4fefbaf781ff53cf6c
ytw9699홈 <ytw9699@naver.com>
2018-12-28 18:03:19 +0900

    간단수정

---
+++ algorithm/CodingInterview/ch_05_Bit_Manipulation/Question2.java
@@ -0,0 +1,29 @@
+package ch_05_Bit_Manipulation;
+
+public class Question2 {
+	public static String printBinary(double num) {
+		if (num >= 1 || num <= 0) {
+			return "error";
+		}
+		StringBuilder binary = new StringBuilder();
+		binary.append(".");
+		while (num > 0) {
+			if (binary.length() > 32) {
+				return "error";
+			}
+			double r = num * 2;
+			if (r >= 1) {
+				binary.append(1);
+				num = r - 1;
+			} else {
+				binary.append(0);
+				num = r;
+			}
+		}
+		return binary.toString();
+	}
+	public static void main(String[] args) {
+		String bs = printBinary(0.75);
+		System.out.println(bs);
+	}
+}
