2725a59a09e37c042405d07a507b7434e7fd62ea
ytw9699홈 <ytw9699@naver.com>
2019-01-09 00:07:28 +0900

    초안

--- algorithm/backjoon/a_2905/Main.java
+++ algorithm/backjoon/a_2905/Main.java
@@ -1,9 +1,33 @@
 package a_2905;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	LinkedList<Integer> queue = new LinkedList();
+	
+	int count = sc.nextInt();
+	
+	int width = sc.nextInt();
+	
+	for(int i =0; i<count; i++) {
+		queue.offer(sc.nextInt());
+	}
+	
+	while(!queue.isEmpty()){
 		
+		for(int j=0; j<width; j++) {
+			queue.poll();
+		}
+		queue.peek();
+		
+	
+	}
+	
+	
 		
 	}
 }
