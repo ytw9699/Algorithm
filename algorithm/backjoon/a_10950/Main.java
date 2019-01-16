1217cd467586a6c3396f5d182192e431388a5087
ytw9699홈 <ytw9699@naver.com>
2019-01-16 17:45:33 +0900

    10950번

---
+++ algorithm/backjoon/a_10950/Main.java
@@ -0,0 +1,14 @@
+package a_10950;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		while (count-- > 0) {
+			 int a= sc.nextInt();
+			 int b= sc.nextInt();
+			 System.out.println(a+b);
+		}
+	}
+}
+
