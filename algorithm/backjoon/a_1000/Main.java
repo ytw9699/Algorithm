b07115c4ce7d645e5305c3d5ce9fe9402a5ffcd8
ytw9699홈 <ytw9699@naver.com>
2019-01-16 17:36:00 +0900

    1000번

--- algorithm/backjoon/a_1000/Main.java
+++ algorithm/backjoon/a_1000/Main.java
@@ -1,11 +1,10 @@
-package a1_입출력받아보기1000번;
+package a_1000;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	int a = sc.nextInt();
     	int b = sc.nextInt();
-    	sc.close();
         System.out.println(a+b);
      }
 }
