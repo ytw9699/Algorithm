945dcadc2eb09f419a05cb9d3944bb51d524e51c
ytw9699홈 <ytw9699@naver.com>
2019-01-16 19:12:13 +0900

    11022

---
+++ algorithm/backjoon/j_11022/Main.java
@@ -0,0 +1,14 @@
+package j_11022;
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 int count = sc.nextInt();
+		 for(int i=1; i<=count; i++) {
+			 int a= sc.nextInt();
+			 int b= sc.nextInt();
+			 System.out.println("Case #"+i+": " +a+" + "+b+" = "+(a+b));
+		}
+	}
+}
