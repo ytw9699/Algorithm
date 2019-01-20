04a60cce696f5fe0f4de61316b2a580e7f36b2b2
태원_홈 <ytw9699@naver.com>
2019-01-21 00:10:37 +0900

    테스팅

--- src/backjoon_백준문제풀이/A_1_testing/Main.java
+++ src/backjoon_백준문제풀이/A_1_testing/Main.java
@@ -6,22 +6,37 @@ public class Main {
     	
        Scanner sc= new Scanner(System.in);
        
-       String Order = sc.nextLine();
+       String first = sc.nextLine();
        
-       String a = null;
-       int input = 10;
-       int length = Order.length();
+       String second[] = first.split(" ");
        
-       if(length > 0 && length <101) {
-       for(int i=0; i<length; i+=input) {
-    	   if(i+input < length) {
-    		   a = Order.substring(i,i+input); 
-    	   }
-    	   else {
-    		    a = Order.substring(i,length); 
-    	   }
-    	   System.out.println(a);
+       while(second[0].equals(second[1]) || second[0].length() != 3 || second[1].length() != 3 || second[0].contains("0") || second[1].contains("0")) {
+    	   first = sc.nextLine();
+    	   second = first.split(" ");
+       }//두수가 같거나 3자리수가 아닐경우 또는 0이 포함될경우 다시 반복한다.
+     
+       String output1[] = second[0].split("");
+       String finals1= "";
+       
+       for(int i=output1.length-1; i>=0; i--) {
+    	   finals1=finals1+output1[i];
        }
+       
+       String output2[] = second[1].split("");
+       String finals2= "";
+       
+       for(int i=output2.length-1; i>=0; i--) {
+    	   finals2=finals2+output2[i];
        }
+       
+        int front = Integer.parseInt(finals1);
+        int rear  = Integer.parseInt(finals2);
+        
+        if(front > rear) {//큰것을 출력하자
+        	System.out.println(front);
+		}
+		else {
+			System.out.println(rear);//큰것을 출력하자
+		}
     }
 }
