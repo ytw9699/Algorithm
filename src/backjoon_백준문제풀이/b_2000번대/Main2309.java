ca0b568a504b2d93870d9f913a203de018ab6a53
태원_홈 <ytw9699@naver.com>
2019-01-27 01:11:25 +0900

    2309번

--- src/backjoon_백준문제풀이/b_2000번대/Main2309.java
+++ src/backjoon_백준문제풀이/b_2000번대/Main2309.java
@@ -1,7 +1,7 @@
-package A_1_testing;
+package b_2000¹ø´ë;
 import java.util.Arrays;
 import java.util.Scanner;
- public class Main {
+ public class Main2309 {
 	public static void main(String[] args)
 	{
 	Scanner sc= new Scanner(System.in);
