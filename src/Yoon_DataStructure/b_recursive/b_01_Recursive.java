a638e9f5e729a98d7c975e58d4428ade09946a75
홈 <ytw9699@naver.com>
2019-02-18 22:24:31 +0900

    주석수정

--- src/Yoon_DataStructure/b_recursive/b_01_Recursive.java
+++ src/Yoon_DataStructure/b_recursive/b_01_Recursive.java
@@ -1,6 +1,8 @@
 package b_recursive;
-	public class b_01_Recursive {
-	static void Recursive(int num) {
+public class b_01_Recursive 
+{
+	static void Recursive(int num) 
+	{
 		if(num <=0)//탈출조건
 			return;//탈출
 		
@@ -8,8 +10,8 @@ package b_recursive;
 		Recursive(num-1);//자기자신을 다시 호출
 		
 	}
-		public static void main(String[] args) {
-				System.out.println(1);
-				Recursive(3);//제일 첫번째로 호출된 메소드 가장 마지막에 종료
+		public static void main(String[] args) 
+		{
+			Recursive(3);//제일 첫번째로 호출된 메소드 가장 마지막에 종료
 		}
- }
+}
