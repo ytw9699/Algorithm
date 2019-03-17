94d4d55fa8ef1b700b1d7a0f9e863c6b3809c611
홈 <ytw9699@naver.com>
2019-03-17 12:54:04 +0900

    수정

--- src/backjoon_백준문제풀이/A_1_testing/Main.java
+++ src/backjoon_백준문제풀이/A_1_testing/Main.java
@@ -1,34 +1,35 @@
 package A_1_testing;
+import java.util.Arrays;
  public class Main {
 	public static void main(String[] args) {
-		int[] capacities=new int[]{10,10,10};//전체 병의 크기
-		int[] bottles=new int[]{3,7,3};//현재들어있는 용량
-		int[] fromld=new int[]{1,0,1};//옮기고 싶은곳의 병
-		int[] told=new int[]{0,1,0};//옮김을 받아야하는 곳의 병
-		
-		int[] a = thePouring(capacities,bottles,fromld,told);
-		
-		for(int i=0; i<a.length; i++) {
-			System.out.println(a[i]);
-		}
-}
+		int[] a = {1,2,3};
+		int[] b = {1,1};
+		System.out.println(solution(a,b));
 	
-	public static int[] thePouring(int[] capacities, int[] bottles, int[] fromld, int[] told){
-		for (int i = 0; i < fromld.length; i++){
-			 int from = fromld[i];
-			 int to = told[i];
-			 int toSpace = capacities[to] - bottles[to];//옮길려는곳의 남은공간
-			 
-			 if (toSpace >= bottles[from]) { //옮기려는 양이 넘치지 않는다면
-				 bottles[to] += bottles[from];
-				 bottles[from] = 0;//병을 비우자
-			 }else{
-				 bottles[to] += toSpace;
-				 bottles[from] -= toSpace;
-			 }
-		}
-			 return bottles;
 }
+	 public static int solution(int[] people, int[] tshirts) {
+		 int answer = 0;
+		 
+         Arrays.sort(people);
+         Arrays.sort(tshirts);
+
+         for(int i=0; i<people.length; i++){
+             for(int j=0; j<tshirts.length; j++){
+                 if(people[i] <= tshirts[j]){
+                	 answer +=1;   
+                	 tshirts[j]=0;
+                	 break;
+                 }
+             }
+         }
+         return answer;
+     }
 }
 
+ 
+ 
+ 
+ 
+
+ 
  
