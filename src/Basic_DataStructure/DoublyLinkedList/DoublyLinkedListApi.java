ed8d7bc7e8c54c7d51233d88f066f3840776143f
홈 <ytw9699@naver.com>
2019-04-03 19:17:32 +0900

    자바 양방향 연결리스트 api

--- src/Basic_DataStructure/DoublyLinkedList/DoublyLinkedListApi.java
+++ src/Basic_DataStructure/DoublyLinkedList/DoublyLinkedListApi.java
@@ -3,20 +3,18 @@ package DoublyLinkedList;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
-public class LinkedListApi {
+public class DoublyLinkedListApi {
 
 	public static void main(String[] args) {
 
 		LinkedList LinkedList = new LinkedList();
 		
 		LinkedList.add(1);
-		LinkedList.add(2);
 		LinkedList.add(3);
+		LinkedList.add(2);
 		LinkedList.add(4);
-		LinkedList.add(6);
-		LinkedList.add(7);
 		LinkedList.add(5);
-		
+	
 		ListIterator i = LinkedList.listIterator();
 		
 		while(i.hasNext()){
@@ -24,13 +22,12 @@ public class LinkedListApi {
      	 }
 		System.out.println();
 		
-		boolean a = LinkedList.remove((Object)2);
+		boolean a = LinkedList.remove((Object)2);//2값을 가진 노드 삭제
 		
-		int index = LinkedList.indexOf(1);//x값의 인덱스를 반환
+		int index = LinkedList.indexOf(1);//x값의 가진 노드의 인덱스를 반환, 0번째 위치 반환
 		
-		LinkedList.add(index,2);//index에 해당하는 값의 앞쪽에 2를 추가
-		LinkedList.add(index+1,2);//index에 해당하는 값의 뒤쪽에 2를 추가
-		
+		LinkedList.add(index,0);//index에 해당하는 위치에 값0을 가진 노드를 추가한다
+		LinkedList.add(index,2);
 		
 		ListIterator k = LinkedList.listIterator();
 		
