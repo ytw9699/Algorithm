ee9f65ee9e6d921b23188a2c30b2f58ff9879c32
홈 <ytw9699@naver.com>
2019-04-08 14:38:03 +0900

    어레이리스트 기반 스택

--- src/Basic_DataStructure/Stack/ArrayListStack.java
+++ src/Basic_DataStructure/Stack/ArrayListStack.java
@@ -1,15 +1,15 @@
 package Stack;
+import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-import Arraylist.ArrayList;
-public class MyStack3 extends ArrayList{//직접 구현 ArrayList기반
+public class ArrayListStack extends ArrayList{//어레이 리스트 기반
 	public Object push(Object data) {
 		add(data);
 		return data;
 	}
 	
 	public Object pop() {
-		Object data = peek();//Stack에 저장된 마지막 요소를 읽어온다.
+		Object data = peek();// Stack에 저장된 마지막 요소를 읽어온다.
 		
 		remove(size() - 1);//마지막 요소를 삭제한다
 		
@@ -19,13 +19,14 @@ public class MyStack3 extends ArrayList{//직접 구현 ArrayList기반
 	public Object peek() {
 		int	len = size();//전체 데이터 갯수
 		
-		if (len == 0) {//데이터 갯수가 없다면
+		if (len == 0) {//전체 데이터 갯수가 없다면
 			throw new EmptyStackException();
+			//EmptyStackException을 발생
 		}
 		return get(len - 1);//마지막 리스트 엘리먼트 리턴	
 		}
 	
 	public boolean empty() {
-		return size() == 0;
+		return size() == 0;//갯수가 0개라면 투르
 		}
 }
--- src/Basic_DataStructure/Stack/ArrayListStackMain.java
+++ src/Basic_DataStructure/Stack/ArrayListStackMain.java
@@ -1,19 +1,19 @@
 package Stack;
 
-public class Main3 {
+public class ArrayListStackMain {
     public static void main(String[] args) {
     	
-    	MyStack3 st = new MyStack3();
+    	ArrayListStack st = new ArrayListStack();
     	
     	st.push("0");
 		st.push("1");
 		st.push("2");
 		
-		System.out.println("= MyStack3 peek=");
+		System.out.println("= MyStack2 peek=");
 		
 		System.out.println(st.peek());//마지막 값 리턴
 		
-		System.out.println("= MyStack3 pop=");
+		System.out.println("= MyStack2 pop=");
 		
 		while(!st.empty()) {
 			System.out.println(st.pop());
